b6facf26e7511fcdfba4cd3a71ea1a050fdd09a8
jason <zahinjason220434@gmail.com>
2019-07-30 17:30:41 +0600

    Exam Create button added

--- app/src/main/java/com/example/examcreate/ExamCreateActivity.java
+++ app/src/main/java/com/example/examcreate/ExamCreateActivity.java
@@ -2,13 +2,32 @@ package com.example.examcreate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ExamCreateActivity extends AppCompatActivity {
 
+    private Button examCreate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exam_create);
+        bindWidgets();
+        bindListeners();
+    }
+
+    private void bindWidgets(){
+        examCreate=findViewById(R.id.create_exam);
+
+    }
+    private void bindListeners(){
+        examCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ExamCreateActivity.this,QuestionCreate.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/examcreate/QuestionCreate.java
@@ -0,0 +1,14 @@
+package com.example.examcreate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuestionCreate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_create);
+    }
+}
