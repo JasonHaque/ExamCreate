ed381961f2ddf73d97a29bd7bfbc5f6c28827682
afia <afiatamanna06@gmail.com>
2019-08-02 19:43:35 +0600

    Timeline imagebutton and background color added

--- app/src/main/java/com/example/examcreate/TimelineActivity.java
+++ app/src/main/java/com/example/examcreate/TimelineActivity.java
@@ -6,10 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class TimelineActivity extends AppCompatActivity {
 
-    private Button addExam;
+    private ImageButton addExam;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
