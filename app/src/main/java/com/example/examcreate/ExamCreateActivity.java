f0b8cf62e7de2402f0688527410fdbd2d84cdbe1
jason <zahinjason220434@gmail.com>
2019-08-06 11:31:33 +0600

    Code added for setting exam names

--- app/src/main/java/com/example/examcreate/ExamCreateActivity.java
+++ app/src/main/java/com/example/examcreate/ExamCreateActivity.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class ExamCreateActivity extends AppCompatActivity {
 
     private Button examCreate;
+    public static String examName;
+    public EditText examSet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,12 +23,14 @@ public class ExamCreateActivity extends AppCompatActivity {
 
     private void bindWidgets(){
         examCreate=findViewById(R.id.create_exam);
+        examSet=findViewById(R.id.exam_name);
 
     }
     private void bindListeners(){
         examCreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                examName=examSet.getText().toString();
                 startActivity(new Intent(ExamCreateActivity.this,QuestionCreate.class));
             }
         });
--- app/src/main/java/com/example/examcreate/QuestionCreate.java
+++ app/src/main/java/com/example/examcreate/QuestionCreate.java
@@ -3,12 +3,23 @@ package com.example.examcreate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import static com.example.examcreate.ExamCreateActivity.examName;
+
 
 public class QuestionCreate extends AppCompatActivity {
 
+    private TextView finalexamName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_create);
+        bindWidgets();
+        finalexamName.setText(""+examName);
+    }
+
+    private void bindWidgets(){
+        finalexamName=findViewById(R.id.set_exam_name);
     }
 }
