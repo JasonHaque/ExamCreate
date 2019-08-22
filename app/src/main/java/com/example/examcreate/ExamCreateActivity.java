d9789a96941588ec69bc2993e28412ac27bb0470
afia <afiatamanna06@gmail.com>
2019-08-22 21:57:47 +0600

    Examcreate activity layout changed

--- app/src/main/java/com/example/examcreate/ExamCreateActivity.java
+++ app/src/main/java/com/example/examcreate/ExamCreateActivity.java
@@ -7,14 +7,16 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class ExamCreateActivity extends AppCompatActivity {
 
     private Button examCreate, backTotimeline;
-    public static String examName;
-    public EditText examSet;
+    public static String examName, examDate;
+    public EditText examSet, dateOfExam;
+    public CheckBox mcqO, writtenO;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,17 +29,21 @@ public class ExamCreateActivity extends AppCompatActivity {
         examCreate=findViewById(R.id.create_exam);
         examSet=findViewById(R.id.exam_name);
         backTotimeline=findViewById(R.id.exam_back);
+        dateOfExam=findViewById(R.id.exam_date);
+        mcqO=findViewById(R.id.mcqid);
+        writtenO=findViewById(R.id.writtenid);
     }
     private void bindListeners(){
         examCreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 examName=examSet.getText().toString();
-                if(TextUtils.isEmpty(examName)){
-                    Toast.makeText(ExamCreateActivity.this,"Fill up the subject of exam properly", Toast.LENGTH_LONG).show();
+                examDate=dateOfExam.getText().toString();
+                if((TextUtils.isEmpty(examName) || TextUtils.isEmpty(examDate) || !mcqO.isChecked()) && (TextUtils.isEmpty(examName) || TextUtils.isEmpty(examDate) || !writtenO.isChecked())){
+                    Toast.makeText(ExamCreateActivity.this,"Fill up the fields properly", Toast.LENGTH_LONG).show();
                     return;
                 }
-                else if(!TextUtils.isEmpty(examName)){
+                else if(!TextUtils.isEmpty(examName) && !TextUtils.isEmpty(examDate) && mcqO.isChecked()){
                     startActivity(new Intent(ExamCreateActivity.this, QuestionCreate.class));
                 }
             }
