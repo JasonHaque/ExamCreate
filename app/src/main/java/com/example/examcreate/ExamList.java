f77c947b66cea09090ff25377fefaa5bdc5e60f2
jason <zahinjason220434@gmail.com>
2019-09-28 00:15:18 +0600

    Added back button on exam list

--- app/src/main/java/com/example/examcreate/ExamList.java
+++ app/src/main/java/com/example/examcreate/ExamList.java
@@ -3,7 +3,10 @@ package com.example.examcreate;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -21,6 +24,7 @@ import static com.example.examcreate.LoginActivity.userID;
 public class ExamList extends AppCompatActivity {
 
     private TextView examListView;
+    private Button backButton;
     private FirebaseFirestore db=FirebaseFirestore.getInstance();
     private CollectionReference dref = db.collection("EXAMS");
     private DocumentReference dhref =dref.document(""+userID);
@@ -29,11 +33,21 @@ public class ExamList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exam_list);
         bindWidgets();
+        bindListeners();
         examListView.setText("Your Exams are Given Below");
 
     }
 
     private void bindWidgets(){
         examListView=findViewById(R.id.exam_list_view);
+        backButton=findViewById(R.id.back_to_timeline);
+    }
+    private void bindListeners(){
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ExamList.this,TimelineActivity.class));
+            }
+        });
     }
 }
